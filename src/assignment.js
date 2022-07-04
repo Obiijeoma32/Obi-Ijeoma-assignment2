f21d5341180a9f91a8c9df55c640f457eb559325
Candy obi ijeoma <98307240+Obiijeoma32@users.noreply.github.com>
2022-07-04 13:14:46 +0000

    Completed challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -4,33 +4,44 @@ const assignment = {};
 // ========================
 
 
-/**
- * Challenge - 1
- * 
- * Calculate and return the sum of the numbers in an array.
- * If you did Challenge - 1, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array of numbers to sum
- * @returns number the sum of the numbers
- */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
 
-/**
- * Challenge - 2
- * 
- * Count all even numbers within an array of numbers.
- * If you did Challenge - 2, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array containing even or non-even numbers
- * @returns number the count of even numbers
- */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+ // * Challenge - 1
+ 
+//  * If you did Challenge - 1, remove the comment in the line just after this function
+//  * 
+//  * @param {Array} arrayOfNumbers the array of numbers to sum
+//  * @returns number the sum of the numbers
+//  */
+function sumOfNumbers(arrayOfNumbers) {
+   var sum = 0;
+  for (var i = 0; i < arrayOfNumbers.length; i++) {
+    sum += arrayOfNumbers[i];
+  } 
+  return sum;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+sumOfNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+assignment.sumOfNumbers = sumOfNumbers;
+
+
+ // * Challenge - 2
+ // * Count all even numbers within an array of numbers.
+
+ // * If you did Challenge - 2, remove the comment in the line just after this function
+ // * 
+ // * @param {Array} arrayOfNumbers the array containing even or non-even numbers
+ // * @returns number the count of even numbers
+ // */
+function countEvenNumbers(arrayOfNumbers) {
+   var block = 0;
+  for (var i = 0; i < arrayOfNumbers.length; i++) {
+    if (arrayOfNumbers[i] % 2 === 0) {
+      block++;
+    }
+  }
+  return block;
+}
+countEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -46,9 +57,9 @@ function countEvenNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
-function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
-}
+// function celsiusToFahrenheit(arrayOfNumbers) {
+//     return arrayOfNumbers;}
+
 // assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
